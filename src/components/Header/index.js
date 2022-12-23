333809c0906943987e86b56cbab2f70f151a27d7
zezerinodev <mattoszz@hotmail.com>
2022-12-23 15:23:13 -0300

    Componente Header finalizado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+//MY IMPORTS
+import './style.css';
 
 function Header(){
     return(
-        <header>Teste Header</header>
+        <header>
+            <Link className='logo' to='/' >Prime Flix</Link>
+            <Link className='favoritos' to='/favoritos' >Meus filmes</Link>
+        </header>
     );
 }
 
