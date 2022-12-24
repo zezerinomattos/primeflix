dce61beed4c38ed791c6a18c1afd23a12cf5ec58
zezerinodev <mattoszz@hotmail.com>
2022-12-24 11:28:43 -0300

    Criando um Loading para aparecer quando estiver aguardando o carregamento.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,6 +9,8 @@ import api from '../../services/api';
 function Home(){
 
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoadig] = useState(true);
+
 
     async function loadFilmes(){
         const response = await api.get('movie/now_playing', {
@@ -20,26 +22,37 @@ function Home(){
         })
         //console.log(response.data.results.slice(0, 10));
         setFilmes(response.data.results.slice(0, 10));
+        setLoadig(false);
     }
 
     useEffect(() => {
         loadFilmes();
     }, []);
 
+
     return(
-        <div className='container'>
-            <div className='lista-filmes'>
-                {filmes.map((filme) => {
-                    return (
-                        <article key={filme.id}>
-                            <strong>{filme.title}</strong>
-                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
-                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
-                        </article>
-                    )
-                })}
-            </div>
-        </div>
+        <>
+            {
+                loading ? 
+                    <div className='loading'>
+                        <h1>Carregando filmes...</h1>
+                    </div>
+                :
+                    <div className='container'>
+                        <div className='lista-filmes'>
+                            {filmes.map((filme) => {
+                                return (
+                                    <article key={filme.id}>
+                                        <strong>{filme.title}</strong>
+                                        <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                                        <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                                    </article>
+                                )
+                            })}
+                        </div>
+                    </div>                
+            }           
+        </>
     );
 }
 
